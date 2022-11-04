cc4de74b480b5605a532a6e2090ca0a9738dd8a3
MIRIANTC <112370207+MIRIANTC@users.noreply.github.com>
2022-11-04 16:23:40 -0500

    Add files via upload

---
+++ Proyecto Final/Proyecto Final-Undiad1/assets/js/main.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    document.getElementById("items-loading").className = "container ocultar";
+    document.getElementById("items-show").className = "container animate__animated  animate__bounce";
+}, 1500);
